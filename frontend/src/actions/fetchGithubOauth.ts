47f9fff5739e4a096bf4604b1b7d114808f586a3
declanleeler <declanlee-ler@hotmail.com>
2025-01-20 01:47:23 +0800

    Fix misspelt endpoint

--- frontend/src/actions/fetchGithubOauth.ts
+++ frontend/src/actions/fetchGithubOauth.ts
@@ -4,7 +4,7 @@ const backendBaseUrl =
   import.meta.env.VITE_BACKEND_URL || 'http://localhost:5173/api';
 
 const fetchGithubOauth = async (code: string) => {
-  const response = await axios.post(`${backendBaseUrl}//oauth/redirect`, {
+  const response = await axios.post(`${backendBaseUrl}/oauth/redirect`, {
     code,
   });
   return response;
