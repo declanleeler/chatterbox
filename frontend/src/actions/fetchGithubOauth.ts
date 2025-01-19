1cb1723cdef2df0343c91c0b8a8e9048a17cc0ac
declanleeler <declanlee-ler@hotmail.com>
2025-01-20 01:45:14 +0800

    Fix oauth server uri

--- frontend/src/actions/fetchGithubOauth.ts
+++ frontend/src/actions/fetchGithubOauth.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const backendBaseUrl =
+  import.meta.env.VITE_BACKEND_URL || 'http://localhost:5173/api';
+
 const fetchGithubOauth = async (code: string) => {
-  const response = await axios.post('/api/oauth/redirect', {
+  const response = await axios.post(`${backendBaseUrl}//oauth/redirect`, {
     code,
   });
   return response;
