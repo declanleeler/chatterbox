82267504620c753b8429b4d1198ed1d500763d1b
declanleeler <declanlee-ler@hotmail.com>
2025-01-20 01:50:53 +0800

    Add middleware

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,8 +1,20 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from .router import router
 from .database import mongo
 
 app = FastAPI()
+origins = [
+    "https://chatterbox-frontend-ochre.vercel.app",  # Production frontend
+    "http://localhost:5173",  # Local frontend (during development)
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allow specified origins
+    allow_credentials=True,  # Allow cookies/auth headers
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers (Authorization, Content-Type, etc.)
+)
 
 
 # Startup and shutdown event handlers
